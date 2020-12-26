bc309f6750f47c002d94e473b3cef6409e415f0f
Che To <chekinto2@hotmail.com>
2020-12-26 21:47:10 +0000

    added javascript mobile navtray and added images for hero

--- js/index.js
+++ js/index.js
@@ -1,7 +1,42 @@
+const header = document.querySelector('header')
 const burger = document.getElementById('burger');
 const line = document.getElementById('line');
+const overlay = document.getElementById('overlay');
+const mobileNav = document.getElementById('mobileNav');
 let isOpen = false;
 
-burger.addEventListener('click', () => {
-  line.classList.toggle('active')
+const toggleMobileNav = (e) => {
+  if (!isOpen) {
+    line.classList.add('active')
+    overlay.classList.add('is-modalOpen')
+    mobileNav.classList.add('is-mobileOpen')
+    document.body.style.overflow = 'hidden'
+    isOpen = true
+  } else {
+    line.classList.remove('active')
+    overlay.classList.remove('is-modalOpen')
+    mobileNav.classList.remove('is-mobileOpen')
+    document.body.style.overflow = 'initial'
+    isOpen = false
+  }
+}
+
+const handleToggleResize = () => {
+  if (window.innerWidth < 800 && isOpen) {
+    line.classList.remove('active')
+    overlay.classList.remove('is-modalOpen')
+    mobileNav.classList.remove('is-mobileOpen')
+    document.body.style.overflow = 'initial'
+    isOpen = false
+  }
+}
+
+burger.addEventListener('click', toggleMobileNav)
+
+overlay.addEventListener('click', (e) => {
+  if (!e.target.contains(overlay)) return
+  document.body.style.overflow = 'hidden'
+  toggleMobileNav()
 })
+
+window.addEventListener('resize', handleToggleResize)
